cf0c92b9c8960bdebe5b9b22985957214245e364
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2021-01-11 19:10:18 -0700

    Built out basic structure for site

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pug = require('pug');
+const path = require('path');
+const route = require('./routes/routes.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+let app = express();
+
+app.use(cors());
+
+app.set('view engine', 'pug');
+app.set('views', __dirname+'/views');
+
+app.use(express.static(path.join(__dirname+'/public')));
+
+let urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
+app.get('/', route.root);
---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const { text } = require('body-parser');
+const e = require('express');
+
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017');
+
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) {
+
+});
+
+var accountSchema = mongoose.Schema({
+    fname: String,
+    lname: String,
+    street: String,
+    city: String,
+    state: String,
+    zip_code: String,
+    email: String,
+    password: Text,
+    phone: String,
+    reviews: Array
+});
+
+var Account = mongoose.model('Account_Collection', accountSchema);
+
+exports.root = (req,res) => {
+    if(err) throw err;
+    res.render('root');
+}
