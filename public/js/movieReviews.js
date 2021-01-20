875689739715b56486783b652d6ddaa768e04f62
DeryGermann <termaiter@gmail.com>
2021-01-19 19:12:50 -0700

    Got star rating to update.

--- public/js/movieReviews.js
+++ public/js/movieReviews.js
@@ -1,7 +1,7 @@
-let parent_div = document.getElementsByClassName('parent-div')
+let conatiner = document.getElementById('container')
 
 const revealReviews = (evt) => {
-    let hiddenBox = evt.target.parentElement.children[1];
+    let hiddenBox = evt.target.parentElement.parentElement.children[1];
 
     if (hiddenBox.style.display === 'none') {
         hiddenBox.style.display = 'block';
@@ -10,6 +10,17 @@ const revealReviews = (evt) => {
     }
 }
 
-for (let i = 0; i < parent_div.length; i++) {
-    parent_div[i].onclick = revealReviews;
+for (let i = 0; i < conatiner.childNodes.length; i++) {
+    conatiner.childNodes[i].childNodes[0].onclick = revealReviews;
+}
+
+// Slider Functionality
+let slider = document.getElementsByClassName('slider');
+
+const updateRatingImage = evt => {
+    evt.target.parentElement.children[1].src = `img/star_rating_${evt.target.value}.png`;
+}
+
+for (let i = 0; i < slider.length; i++) {
+    slider[i].onchange = updateRatingImage;
 }
