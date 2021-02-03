2938bac7d80ef488c61b8533b5dff6ffba510d9d
Eric Young <eyoung88@frontier.com>
2021-02-02 19:30:07 -0700

    captcha and some signup

--- Final/movie_app/src/components/header.js
+++ Final/movie_app/src/components/header.js
@@ -3,10 +3,12 @@ const Header = props => {
     let moviePage;
     let accountPage;
     let adminPage;
+    let signUpPage;
 
     if (sessionStorage.getItem('user')) {
         moviePage = <a href='/movies'>Movies</a>
         accountPage = <a href='/account'>Account Settings</a>
+        signUpPage = <a href='/signup'>Sign Up</a>
 
         // Checks if the user is an admin
         let isAdmin = JSON.parse(sessionStorage.getItem('user')).isAdmin;
@@ -22,6 +24,7 @@ const Header = props => {
                 { moviePage }
                 { accountPage }
                 { adminPage }
+                { signUpPage }
                 <a href='/' onClick={() => { sessionStorage.removeItem('user') }}>Log Out</a>
             </div>
         </>
